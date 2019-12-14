702c4cd68f75c5ffad5d7ba2c48c7499541d742a
Danilo Requena <danilo.requena26@gmail.com>
2019-12-13 22:27:55 -0300

    Initial Commit

---
+++ CupChampions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CupChampions
+//
+//  Created by Danilo Requena on 13/12/19.
+//  Copyright © 2019 Danilo Requena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
