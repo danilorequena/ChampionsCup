a85f33c1a79d382aa13543014924116ced404316
Danilo Requena <danilo.requena26@gmail.com>
2019-12-25 09:06:29 -0300

    Cria as telas, model e controllers

---
+++ CupChampions/Controllers/WorldCupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WorldCupViewController.swift
+//  CupChampions
+//
+//  Created by Danilo Requena on 22/12/19.
+//  Copyright © 2019 Danilo Requena. All rights reserved.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CupChampions/Models/WorldCup.swift
@@ -0,0 +1,9 @@
+//
+//  WorldCup.swift
+//  CupChampions
+//
+//  Created by Danilo Requena on 25/12/19.
+//  Copyright © 2019 Danilo Requena. All rights reserved.
+//
+
+import Foundation
