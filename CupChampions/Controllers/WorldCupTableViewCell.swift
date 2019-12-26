3a2d0c234d8ab784913866419eb62268b6da4cf8
Danilo Requena <danilo.requena26@gmail.com>
2019-12-26 00:46:40 -0300

    Controi a celula customizada

---
+++ CupChampions/Controllers/WorldCupTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  WorldCupTableViewCell.swift
+//  CupChampions
+//
+//  Created by Danilo Requena on 26/12/19.
+//  Copyright © 2019 Danilo Requena. All rights reserved.
+//
+
+import UIKit
+
+class WorldCupTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbYear: UILabel!
+    @IBOutlet weak var ivWinner: UIImageView!
+    @IBOutlet weak var ivVice: UIImageView!
+    @IBOutlet weak var lbWinner: UILabel!
+    @IBOutlet weak var lbVice: UILabel!
+    @IBOutlet weak var lbWinnerScore: UILabel!
+    @IBOutlet weak var lbViceScore: UILabel!
+    @IBOutlet weak var lbCountry: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func prepare(with cup: WorldCup){
+        lbYear.text = "\(cup.year)"
+        ivWinner.image = UIImage(named: "\(cup.winner)")
+        ivVice.image = UIImage(named: "\(cup.vice)")
+        lbWinner.text = cup.winner
+        lbVice.text = cup.vice
+        lbWinnerScore.text = cup.winnerScore
+        lbViceScore.text = cup.viceScore
+        lbCountry.text = cup.country
+    }
+
+}
